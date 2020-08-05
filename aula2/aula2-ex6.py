94ab84718c7756a70ad9627b9750317915ec5b11
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2020-08-05 11:52:42 -0300

    sexto execicio da aula2 - atualizacao

--- aula2/aula2-ex6.py
+++ aula2/aula2-ex6.py
@@ -7,20 +7,17 @@ numero2 = float(input("\n Informe o segundo numero:"))
 numero3 = float(input("\n Informe o terceiro numero:"))
 
 if numero1 > numero2 :
-    if numero1> numero3 :
+    if numero1 > numero3:
         print("\n O maior é o primeiro:", numero1)
-    elif numero1 == numero3 :
-        print("\n Há dois maiores, o um e o tres:", numero1, " / ", numero3)
-    else :
-        print("\n O maior é o terceiro:", numero3)
-elif numero2 > numero3 :
-    print("\n O maior é o segundo:", numero2)
-elif numero2 == numero3 :
-    print("\n Há dois maiores, o dois e o tres:", numero2, " / ", numero3)
-elif numero1 == numero2 :
-    print("\n Há dois maiores, o um e o dois:", numero1, " / ", numero2)
-elif numero3 > numero2 :
-    print("\n O maior é o terceiro:", numero3)
+if numero2 > numero1 :
+    if numero2 > numero3 :
+        print("\n O maior é o segundo:", numero2)
+if numero3 > numero1 :
+    if numero3 > numero2 :
+        print("\n O maior é o segundo:", numero3)
+if numero1 == numero2 == numero3 :
+    print( "\n Os tres numeros sao iguais")
+
 
 
 
