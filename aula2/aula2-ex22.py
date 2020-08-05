27b30eabb977adee68a19fae27880ed151c803fc
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2020-08-05 13:33:15 -0300

    Acrescimo Ex: 22 - aula2

---
+++ aula2/aula2-ex22.py
@@ -0,0 +1,9 @@
+# aula2 - ex22 - Arnott Ramos Caiado
+# entrar com numero e identificar se eh par ou impar
+
+print("\n Testo se um numero inmteiro e' par ou impar")
+numero = int ( input("\n Entrar com Numero:"))
+if numero % 2 == 0 :
+    print( "\n Numero par: ", numero )
+else :
+    print( "\n Numero impar: ", numero )
