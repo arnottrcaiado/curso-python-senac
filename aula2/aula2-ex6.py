e709a8fd8bf91897572900bb2c5391f77aa1c92e
arnottrcaiado <arnottrcaiado@fac.pe.senac.br>
2020-08-05 12:02:42 -0300

    Atualizacao exercicio 6 e add ex 7

--- aula2/aula2-ex6.py
+++ aula2/aula2-ex6.py
@@ -14,7 +14,7 @@ if numero2 > numero1 :
         print("\n O maior é o segundo:", numero2)
 if numero3 > numero1 :
     if numero3 > numero2 :
-        print("\n O maior é o segundo:", numero3)
+        print("\n O maior é o terceiro:", numero3)
 if numero1 == numero2 == numero3 :
     print( "\n Os tres numeros sao iguais")
 
